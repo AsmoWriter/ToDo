fbb6b8a450a9c38442f8a50b7dfd4e4f2f6685e3
AsmoWriter <lyavolpe@gmail.com>
2023-12-03 15:50:29 +0300

    create todo and admin

--- backend_api/core/views.py
+++ backend_api/core/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+from .models import Todo
+from .serializers import TodoSerializer
+
 
 def home(request):
     return render(request, 'home.html')
+
+
+@api_view(['GET', 'POST'])
+def todos_list(request):
+    todos = Todo.objects.all()
+    serializer = TodoSerializer(todos, many=True)
+    return Response(serializer.data)
